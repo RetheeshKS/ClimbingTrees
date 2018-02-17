10097c63e66f07a773b0c4de8870ea46846b381b
RetheeshKS <36557071+RetheeshKS@users.noreply.github.com>
2018-02-17 13:49:16 +0530

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -1,6 +1,11 @@
 #include <common.h>
 int BST_InsertNode(struct node *root, struct node *newnode)
 {
+  if(!root){
+    return FALSE;
+  }
+  if(GetData(root) >= GetData(newnode)){
+    
 }
 int BST_SerachData(struct node *root, int data)
 {
