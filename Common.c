9cc8af002ae350aa56beb10827da1f6da7aa1be2
RetheeshKS <36557071+RetheeshKS@users.noreply.github.com>
2018-02-17 09:50:09 +0530

    Update Common.c

--- Common.c
+++ Common.c
@@ -1,1 +1,38 @@
 
+struct node {
+  int data;
+  int balancefactor;
+  struct node *left, *right;
+};
+typedef struct node datanode;
+
+typedef enum {
+  FALSE = 0,
+  TRUE,
+}boolean;
+boolean InitNode(datanode *node)
+{
+  if (!node){
+    return FALSE
+  }
+  memset(node, 0, sizeof(datanode))
+  return TRUE;
+}
+
+boolean SetData(datanode * node, int data)
+{
+  if(!node){
+    return FALSE;
+  }
+  node -> data = data;
+  return TRUE;
+}
+
+datanode * GetNewNode(int data)
+{
+  datanode temp = (datanode *)malloc(sizeof(datanode));
+  if (InitNode(temp)){
+    SetData(temp, data);
+  }
+  return temp;
+}
