1a2150345ae3c9177d096146cc649c44c344ef05
RetheeshKS <36557071+RetheeshKS@users.noreply.github.com>
2018-02-17 10:08:39 +0530

    Update Common.h

--- Common.h
+++ Common.h
@@ -1,1 +1,19 @@
 
+typedef enum {
+  FALSE = 0,
+  TRUE,
+}boolean;
+
+struct node {
+  int data;
+  int balancefactor;
+  struct node *left, *right;
+};
+typedef struct node datanode;
+
+datanode * GetLeft(datanode * parent)
+datanode * GetRight(datanode * parent)
+int GetData(datanode * node)
+boolean SetRight(datanode * parent, datanode * right)
+boolean SetLeft(datanode * parent, datanode * left)
+boolean SetData(datanode * node, int data)
