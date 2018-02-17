4e679b681eabf318d49d42b0ac02f410b9ae31a5
RetheeshKS <36557071+RetheeshKS@users.noreply.github.com>
2018-02-17 10:11:14 +0530

    Update Common.h

--- Common.h
+++ Common.h
@@ -11,9 +11,10 @@ struct node {
 };
 typedef struct node datanode;
 
-datanode * GetLeft(datanode * parent)
-datanode * GetRight(datanode * parent)
-int GetData(datanode * node)
-boolean SetRight(datanode * parent, datanode * right)
-boolean SetLeft(datanode * parent, datanode * left)
-boolean SetData(datanode * node, int data)
+datanode * GetLeft(datanode * parent);
+datanode * GetRight(datanode * parent);
+int GetData(datanode * node);
+boolean SetRight(datanode * parent, datanode * right);
+boolean SetLeft(datanode * parent, datanode * left);
+boolean SetData(datanode * node, int data);
+datanode * GetNewNode(int data);
