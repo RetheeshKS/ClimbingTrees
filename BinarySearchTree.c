61a1691c947ce8e03433131aa54a65e1c257c07d
RetheeshKS <36557071+RetheeshKS@users.noreply.github.com>
2018-02-17 13:52:02 +0530

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -5,6 +5,18 @@ int BST_InsertNode(struct node *root, struct node *newnode)
     return FALSE;
   }
   if(GetData(root) >= GetData(newnode)){
+    if(GetLeft(root)){
+      BST_InsertNode(GetLeft(root), newnode);
+    } else{
+      SetLeft(root, newnode);
+    } 
+  } else {
+    if(GetRight(root)){
+      BST_InsertNode(GetRight(root), newnode);
+    } else{
+      SetRight(root, newnode);
+    }
+  }
     
 }
 int BST_SerachData(struct node *root, int data)
