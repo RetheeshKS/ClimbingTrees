b7babce7cb94bfbd49daa2889f29b051544d3ea3
RetheeshKS <36557071+RetheeshKS@users.noreply.github.com>
2018-03-01 06:42:13 +0530

    Update AVLTree.c

--- AVLTree.c
+++ AVLTree.c
@@ -7,8 +7,14 @@ void AVL_BalanceTree(datanode *GGP, datanode *GP)
 
 datanode * AVL_InsertNode(datanode *root, int data)
 {
+  datanode *parent = NULL;
   if (!root){
     root = GetNewNode(data);
   }
-  
+  parent = BST_GetParentNode(root, data);
+  if(GetData(parent) >= data){
+    SetLeft(parent, GetNewNode(data));
+  } else {
+    SetRight(parent, GEtNewNode(data));
+  }
 }
